f26e3dc41d13d6483bb5677254b7736ef64c0135
Luiz Amorim <luiz.simples@gmail.com>
2016-03-10 19:01:51 -0300

    Add username field into form

---
+++ __tests__/VerifyFormFields.test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+jest.unmock('../src/LoginForm');
+
+import LoginForm from '../src/LoginForm';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
+
+describe('Verify form fields', () => {
+  it ('check exist form elements', () => {
+    const loginForm = TestUtils.renderIntoDocument(
+      <LoginForm />
+    );
+
+    const loginFormDOM = ReactDOM.findDOMNode(loginForm);
+    const fieldUsername = loginFormDOM.querySelector('input[name="username"]');
+
+    expect(fieldUsername).not.toBeNull();
+  });
+
+  // it('changes the text after click', () => {
+  //   // Render a checkbox with label in the document
+  //   const checkbox = TestUtils.renderIntoDocument(
+  //     <CheckboxWithLabel labelOn="On" labelOff="Off" />
+  //   );
+  //
+  //   const checkboxNode = ReactDOM.findDOMNode(checkbox);
+  //
+  //   // Verify that it's Off by default
+  //   expect(checkboxNode.textContent).toEqual('Off');
+  //
+  //   // Simulate a click and verify that it is now On
+  //   TestUtils.Simulate.change(
+  //     TestUtils.findRenderedDOMComponentWithTag(checkbox, 'input')
+  //   );
+  //   expect(checkboxNode.textContent).toEqual('On');
+  // });
+});
---
+++ src/LoginForm.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+export default class LoginForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <input name="username" />
+      </form>
+    );
+  }
+}
